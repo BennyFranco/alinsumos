bfd1b2a8253b9d96bcf0bf29631f3b756ba60f24
fenix <betzy_skamm@hotmail.com>
2018-06-04 22:43:24 -0700

    added link facebook to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -19,6 +19,15 @@
             </div>
             <div class="col-md-4">
                 <h6>Enlaces</h6>
+                 <ul>
+                    <li class="text-custom">
+                        <a href="https://www.facebook.com/alinsumos/"><i class="fab fa-facebook-square"></i>Facebook</a>
+                        </li>
+
+                    <li><a href="tel:+52 (644) 445 01 17"> </a></li>
+                    <li><a href="tel:+52 (644) 445 02 51"> </a></li>
+                    <li><a href="tel:+52 (644) 445 01 16"> </a></li>
+                </ul>
             </div>
         </div>
         <a href="#" title="Inicio" v-scroll-to="'#top'">
@@ -47,4 +56,11 @@ svg.svg-inline--fa.fa-chevron-up.fa-w-14 {
   margin-bottom: 20px;
   color: #884f20;
 }
+
+svg.svg-inline--fa.fa-facebook-square.fa-w-14 {
+    margin-right: 5%;
+    font-size: 35px;
+    vertical-align: middle;
+}
+
 </style>
