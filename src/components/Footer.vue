5b3a15adcecb043e4a5d4993a94bb88d7ea980f7
fenix <betzy_skamm@hotmail.com>
2018-07-03 13:06:29 -0700

    changed footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,35 @@
 <template>
 <div>
-<footer class="container-fluid text-center">
+<footer class="container align-class-center  text-center">
+    <div class="row align-vertical-class  align-items-center">
+        <div class="col-12">
+            <address class="text-center">
+                <strong>Alinsumos, S.A. de C.V.</strong><br> Carretera internacional México-Nogales. Km 1841 S/N. Parque industrial. Cd. Obregón, Sonora, México.<br/>
+            </address>
+        </div>
+    </div>
+
+    <div class="row align-vertical-class  align-items-center">
+        <div class="col-8 ">
+            <i class="fa fa-phone mr-3"></i><a href="tel:+52 (644) 413 99 29"> +52 (644) 413 99 29 </a> | <a href="tel:+52 (644) 445 01 17"> +52 (644) 445 01 17</a> | <a href="tel:+52 (644) 445 02 51"> +52 (644) 445 02 51</a> | <a href="tel:+52 (644) 445 01 16"> +52 (644) 445 01 16</a>
+        </div>
+        <div class="col-3 ">
+            <a href="https://www.facebook.com/alinsumos/"><i class="fab fa-facebook-square"></i>Facebook</a>
+        </div>
+    </div>
+     <hr>
+    <div class="row  align-class-center align-items-center">
+        <div class="col-6 "></div>
+        <div class="col-12 animation">
+            <a href="#" title="Inicio" v-scroll-to="'#top'">
+                <i class="fas fa-chevron-up"></i>
+            </a>
+        </div>
+        <div class="col-6"></div>
+
+    </div>
+</footer>
+<!--<footer class="container-fluid text-center">
         <div class="row">
             <div class="col-md-4">
             <address class="text-left">
@@ -34,6 +63,7 @@
             <i class="fas fa-chevron-up"></i>
         </a>
     </footer>
+-->
 </div>
 </template>
 
@@ -50,7 +80,12 @@ li {
 * {
   font-size: small;
 }
-
+address{
+    margin-bottom: 0px;
+}
+hr{
+    margin: 5px;
+}
 svg.svg-inline--fa.fa-chevron-up.fa-w-14 {
   font-size: 20px;
   margin-bottom: 20px;
@@ -62,5 +97,62 @@ svg.svg-inline--fa.fa-facebook-square.fa-w-14 {
     font-size: 35px;
     vertical-align: middle;
 }
+.align-class{
+    padding-top:10px;
+    padding-bottom: 5px;
+}
+.align-vertical-class{
+    padding-left: 10%;
+     padding-right: 10%;
+}
+.color{
+    background-color: #F0D4B9;
+}
+.animation{
 
+    -webkit-animation-name: example;/* Safari 4.0 - 8.0 */
+    -webkit-animation-duration: 6s;
+    -webkit-animation-iteration-count: infinite;
+
+    animation-name: example;/* Chrome, Safari, Opera */
+    animation-duration: 9s;
+    animation-iteration-count: infinite;
+}
+/* Safari 4.0 - 8.0 */
+@-webkit-keyframes example {
+  0% {
+    background-color:#FFFFFF; bottom:0px; top:20px;
+  }
+  25% {
+    background-color:#FFEBD4; bottom:0px; top:0px;
+  }
+  50% {
+    background-color:#FFDEB8; bottom:0px; top:0px;
+  }
+  75% {
+    background-color:#FFEBD4; bottom:0px; top:0px;
+  }
+  100% {
+  background-color:#FFFFFF; bottom:20px; top:0px;
+  }
+}
+
+/* Standard syntax */
+@keyframes example {
+  0% {
+    background-color:#FFFFFF; bottom:0px; top:20px;
+  }
+  25% {
+    background-color:#FFEBD4; bottom:0px; top:0px;
+  }
+  50% {
+    background-color:#FFDEB8; bottom:0px; top:0px;
+  }
+  75% {
+    background-color:#FFEBD4; bottom:0px; top:0px;
+  }
+  100% {
+  background-color:#FFFFFF; bottom:20px; top:0px;
+  }
+}
 </style>
